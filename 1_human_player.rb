0193d0680f04c66b7a69613c8e94a644b2f745e7
Eduardo Gonzalez <edugonzavila@gmail.com>
2022-03-21 19:41:30 -0700

    Modify HumanPlayer#get_position to use cursor input

--- 1_human_player.rb
+++ 1_human_player.rb
@@ -9,31 +9,11 @@ class HumanPlayer
     attr_reader :mark
 
     def get_position(legal_positions)
-        puts "Player #{@mark}'s move:"
-        puts "Enter a position, in a 'row,column' format. Should only contain two numbers separated by a comma:"
-        input = gets.chomp
-        abc = ("a".."z").to_a
-        # Invalid if letters in the position
-        input.each_char do |ch|
-            if abc.include?(ch)
-                puts "Invalid position! Should not include letters"
-                return nil
+        result = nil
+            until result
+                @display.render
+                result = @display.get_input
             end
-        end
-        # Invalid if given more than two coords
-        if input.split(",").length != 2
-            puts "Invalid position! Should provide two noumbers separated by a comma, ex: 0,2"
-            return nil
-        end
-        position = input
-            .split(",")
-            .map(&:to_i)
-
-        if !legal_positions.include?(position)
-            puts "Illegal position, either out of bounds or already occupied"
-            return nil
-        end
-
-        return position
+            result
     end
 end
